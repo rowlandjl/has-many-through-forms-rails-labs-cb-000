03efc8df1d673a5d3c7fb82ebdd3caa1b9db7312
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-04 18:20:43 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,8 @@ class Post < ActiveRecord::Base
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
-      self.categories << category 
-    end 
-  end 
+      self.categories << category
+    end
+  end
 
 end
